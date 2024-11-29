8c6e5ed0355fee32df95142565759c7a57438d89
Jeison Rosario <jsrosario97@gmail.com>
2024-11-29 15:28:35 -0400

    prueba unitaria y docker

--- dockerfile
+++ dockerfile
@@ -1,5 +1,5 @@
 FROM nginx:alpine
 
-COPY src/Final.html /usr/share/nginx/html
+COPY src/index.html /usr/share/nginx/html
 
 EXPOSE 80
--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { JSDOM } = require('jsdom');
 
 test('La página debe contener "Hola mundo DevOps práctica final."', () => {
-    const html = fs.readFileSync(path.resolve(__dirname, 'src', 'Final.html'), 'utf8');
+    const html = fs.readFileSync(path.resolve(__dirname, 'src', 'index.html'), 'utf8');
     const dom = new JSDOM(html);
     const h1 = dom.window.document.querySelector('h1');
     expect(h1.textContent).toBe('Hola mundo DevOps práctica final.');
